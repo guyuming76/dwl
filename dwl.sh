3c5edbda1e22a3e8f17411eb0672a813c6fc1dae
guyuming76 <gu_yu_ming@hotmail.com>
2023-05-31 09:41:37 +0800

    add dwl launch scripts into package

---
+++ dwl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+~/xdg_run_user
+
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+export GTK_IM_MODULE="wayland"
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+dbus-run-session dwl -s ~/dwlstart.sh -d  2>/tmp/dwl.log
+#WAYLAND_DEBUG=server dbus-run-session dwl -s ~/dwlstart.sh -d  2>/tmp/dwl.log
+#dbus-run-session dwl -s ~/dwlstart.sh -d 2>~/dwl.log
+
+#dbus-run-session dwl
