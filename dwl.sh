63668a6c5905ae6eed431b1382605a06123e8c87
guyuming76 <gu_yu_ming@hotmail.com>
2023-06-05 14:38:31 +0800

    add set -x so user will see what have been run. There is sudo in script and will prompt for password

--- dwl.sh
+++ dwl.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+set -x
+
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 xdg_run_user
 
-export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
 export GTK_IM_MODULE="wayland"
 export _JAVA_AWT_WM_NONREPARENTING=1
 
-#export WLR_NO_HARDWARE_CURSORS=1
-#in my virtualbox guest, i need to have the above uncommented, otherwise, cursor won't display in dwl
+echo "export WLR_NO_HARDWARE_CURSORS=1    # try this if you don't see cursor in dwl"
+#in my virtualbox guest cursor won't display in dwl. i need to set this to fix
 
 dbus-run-session dwl -s /usr/bin/dwlstart.sh  2>/tmp/dwl.log
 #WAYLAND_DEBUG=server dbus-run-session dwl -s ~/dwlstart.sh -d  2>/tmp/dwl.log
